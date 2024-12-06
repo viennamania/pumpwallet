36a188974f8c6763d492e9635b90f066ba7854d6
seunghyonpark <creath.park@gmail.com>
2024-12-06 11:09:01 +0900

    refactor: clean up getAccountOkx API route by removing commented code and restructuring response handling

--- src/app/api/agent/getAccountOkx/route.ts
+++ src/app/api/agent/getAccountOkx/route.ts
@@ -45,116 +45,13 @@ export async function POST(request: NextRequest) {
    } = body;
   
 
-   return NextResponse.json({
-    result: {
-        status: "ok",
-        data: {}
-    },
-});
-
-
-    //const HOST = 'api.huobipro.com';
-    
-    const HOST = 'api.huobi.pro';
-
-    ///const HOST = 'hapi.hbdm.com';
-
-
-
-    const DEFAULT_HEADERS = {
-      "Content-Type": "application/json",
-      "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36"
-    };
-
-
-    const path = `/v1/account/accounts`;
-
-
-
-    ////const path = "/api/v1/contract_account_info";
-
-
-    const huobiBody: { [key: string]: string | number | undefined } = {
-        AccessKeyId: htxAccessKey,
-        SignatureMethod: "HmacSHA256",
-        SignatureVersion: 2,
-        Timestamp: moment.utc().format('YYYY-MM-DDTHH:mm:ss'),
-    };
-
-
-    // var payload = sign_sha('GET', HOST, path, huobiBody);
-
-    //const payload = sign_sha('GET', HOST, path, huobiBody);
-
-    try {
-
-
-      var p = '';
-
-      var pars = [];
-      for (let item in huobiBody) {
-          const value = huobiBody[item];
-          if (value !== undefined) {
-              pars.push(item + "=" + encodeURIComponent(value));
-          }
-          p = pars.sort().join("&");
-          var meta = ['GET', HOST, path, p].join('\n');
-          
-          var hash = HmacSHA256(meta, htxSecretKey);
-
-          var Signature = encodeURIComponent(CryptoJS.enc.Base64.stringify(hash));
-          p += `&Signature=${Signature}`;
-      }
-
-      console.log(p);
-
-
-
-
-      const response = await fetch(`https://${HOST}${path}?${p}`, {
-          method: 'GET',
-          headers: DEFAULT_HEADERS,
-      });
-
-      if (!response) {
-          return NextResponse.json({
-              result: {
-                  status: "error",
-              },
-          });
-      }
-
-      const data = await response.json();
-
-      console.log(data);
-
-
-      /*
-      {
-        status: 'ok',
-        data: [ { id: 63912897, type: 'spot', subtype: '', state: 'working' } ]
-      }
-      */
-
-      if (data.status === 'ok') {
-          return NextResponse.json({
-              result: {
-                  status: "ok",
-                  data: data.data
-              },
-          });
-      }
-      
-      
-    } catch (error) {
-        console.error("error", error);
-    }
-
- 
     return NextResponse.json({
-      result: {
-          status: "error",
-      },
+        result: {
+            status: "ok",
+            data: {}
+        },
     });
+
+
   
 }
